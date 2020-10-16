26d1c87c309ce65a325a6c81834bb0d1b3c560d3
miguehernaandez <miguel.08.144@gmail.com>
2020-10-16 15:39:45 -0300

    Agregada ruta para retornar todas las ordenes

--- api/src/routes/order.js
+++ api/src/routes/order.js
@@ -1,16 +1,35 @@
 const server = require('express').Router(); //Import router from express module.
 const { OK, CREATED, UPDATED, ERROR, NOT_FOUND, ERROR_SERVER } = require('../constants'); // Import Status constants.
 const { Order } = require('../db.js'); // Import Products model.
-const {status} = require('sequelize'); // Import operator from sequelize module.
+// const {status} = require('sequelize'); // Import operator from sequelize module.
 
 
 
 // Start routes
 //// 'Get order' route in '/?query={value}'
-server.get('/', ( req, res ) => {
+
+server.post ('/',( req, res ) => {
+	const { key, price, quantity, total, date, status } = req.body;
+
+	return Order.create({ key, price, quantity, total, date, status })
+		.then((order) => {
+			return res.status(OK).json({
+				message: 'Orden creada exitosamente!',
+				data: order,
+			});
+		})
+		.catch((err) => {
+			return res.status(ERROR).json({
+				message: 'Error al crear la orden',
+				data: err,
+			});
+		});
+});
+
+server.get ('/',( req, res ) => {
 	Order.findAll({
 		where: {
-			status: 'Open'
+			status: req.query.status
 		}
 	}) 
 		.then(orders => {
