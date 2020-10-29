89259827f665599f9bd349d5eb6e2824552123de
Cristovk <63594174+Cristovk@users.noreply.github.com>
2020-10-29 18:40:02 -0300

    Contacto fix

--- client/src/App.js
+++ client/src/App.js
@@ -13,38 +13,20 @@ import ProductCard from './components/ProductCard/index';
 import Catalogo from './components/Catalogo/index';
 import FormUsers from './components/FormUsers/FormUsers.jsx';
 import UsersData from './components/AdminForm/UsersData';
-import UserDetaul from './components/AdminForm/DetailUser.jsx'
-import Contacto from './components/Contacto/Contacto.js';
+import UserDetail from './components/AdminForm/DetailUser.jsx';
+import Faqs from './components/FAQs/Faqs.jsx';
 // Bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch, Link } from 'react-router-dom';
 import axios from 'axios';
-import CartShop from './components/Cart/card';// Redux
+import CartShop from './components/Cart/card'; // Redux
+import Login from './components/Login/Login';
+import ProfileUser from './components/ProfileUser/ProfileUser';
+import PrivateAdmin from './components/Routes/PrivateAdmin';
+import PrivateRoute from './components/Routes/PrivateRoute';
+import { enlacesUser, enlacesUserConAdmin, enlacesUserSinAdmin, enlacesAdmin } from './constans/constans';
 
-
-
-
-
-
-const url = 'localhost:3001';
-
-var enlacesUser = [
-	{ text: 'Catalogo', to: '/products/catalogo' },
-	{ text: 'FAQs', to: '/faqs' },
-	{ text: 'Contacto', to: '/contacto' },
-	{ text: 'Ayuda', to: '/' },
-	// { text: 'Registro', to: '/users' }, // Por ahora para probar nomas
-	{ text: 'ADMIN', to: '/admin' },
-];
-
-var enlacesAdmin = [
-	{ text: 'Inicio', to: '/admin' },
-	{ text: 'Usuarios', to: '/admin/users' },
-	{ text: 'Categorías', to: '/admin/category' },
-	{ text: 'Productos', to: '/admin/product' },
-	{ text: 'Ordenes', to: '/admin/orders' }
-];
-
+const url = 'development.c4vdw28snupv.sa-east-1.rds.amazonaws.com:5432';
 
 function App() {
 	const [products, setProduct] = useState([]);
@@ -62,44 +44,77 @@ function App() {
 
 	return (
 		<div>
-			{/* <ProductCard/> */}
 			<Switch>
+				{/* HOME */}
 				<Route path='/' exact>
-					<Navegacion links={enlacesUser} showSearchbar={true} onSearch={onSearch} />
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
 					<Slider />
 					<Footer></Footer>
-					{/* <FormUsers></FormUsers> */}
+				</Route>
+				{/* HOME */}
+
+				{/* FAQS */}
+				<Route path='/faqs' exact>
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
+					<Faqs></Faqs>
+					<Footer></Footer>
 				</Route>
 
-				<Route path='/contacto' exact component={Contacto} />
-				{/* <Route path='/admin' exact > */}
+				{/* FAQS */}
+
+				{/* ADMIN */}
 				<Route path='/admin'>
-					<Navegacion links={enlacesAdmin} showSearchbar={false} />
-					<PrinciapalAdmin />
-					<Route path='/admin' exact>
-						<WellcomeAdmin />
-					</Route>
-					<Route path='/admin/product' component={Product} />
-					<Route path='/admin/category' component={Category} />
-					<Route path='/admin/users' component={UsersData} />
-					<Route path='/admin/orders' component={Orders} />
+					<PrivateAdmin path='/admin' exact component={WellcomeAdmin} />
+					<PrivateAdmin path='/admin/product' component={Product} />
+					<PrivateAdmin path='/admin/category' component={Category} />
+					<PrivateAdmin path='/admin/users' component={UsersData} />
+					<PrivateAdmin path='/admin/orders' component={Orders} />
 				</Route>
+				{/* ADMIN */}
+
+				{/* PRODUCTS */}
+				<Route path='/products/catalogo'>
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
+					<Catalogo products={products} onSearch={onSearch} />
+					<Footer></Footer>
+				</Route>
+
+				<Route path='/products/product/:id'>
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} />
+					<ProductDet />
+				</Route>
+				{/* PRODUCTS */}
+
+				{/* USERS */}
 				<Route path='/users' exact>
-					<Navegacion links={enlacesUser} showSearchbar={true} onSearch={onSearch} />
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
 					<FormUsers></FormUsers>
 					<Footer></Footer>
 				</Route>
-				<Route path='/products/product/:id'>
-					<Navegacion links={enlacesUser} showSearchbar={true} />
-					<ProductDet />
+
+				<Route path='/users/cart'>
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
+					<CartShop></CartShop>
+					<Footer></Footer>
 				</Route>
-				<Route path='/users/cart' component={CartShop} />
-				<Route path='/products/catalogo' render={() => <Catalogo products={products} onSearch={onSearch} />}></Route>
-				<Route path='/users/:id' component={UserDetaul}/>
+
+				<Route path='/users/:id' component={UserDetail} />
+				{/* USERS */}
+
+				{/* PROFILE */}
+				<PrivateRoute path='/profile' component={ProfileUser} />
+				{/* PROFILE */}
+
+				{/* LOGIN */}
+				<Route path='/login' exact>
+					<Navegacion linksU={enlacesUserSinAdmin} linksA={enlacesUserConAdmin} showSearchbar={true} onSearch={onSearch} />
+					<Login></Login>
+					<Footer></Footer>
+				</Route>
+				{/* LOGIN */}
 			</Switch>
 		</div>
 	);
 }
-
 
 export default App;
