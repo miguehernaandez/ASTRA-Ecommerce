57baca67a98ce06c3521966e992292430a59a679
Martin <martincatala14@gmail.com>
2020-11-02 12:01:43 -0300

    Cambiaron los estilos del LOGIN

--- client/src/components/Modals/LoginModals.jsx
+++ client/src/components/Modals/LoginModals.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-import s from '../../styles/avisoLoggin.module.css';
+import s from '../../styles/loginModals.module.css';
 import { Button, Modal, Row, Col, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 export const LoginModalNoUser = ({ showModalNoUser, setShowModalNoUser }) => {
 	return (
 		<div>
-			<Modal centered show={showModalNoUser} onHide={() => setShowModalNoUser(false)} dialogClassName='modal-90w' aria-labelledby='example-custom-modal-styling-title'>
-				<Modal.Header className={s.title} closeButton>
-					<Modal.Title>El usuario ingresado no se encuentra registrado</Modal.Title>
+			<Modal centered show={showModalNoUser} onHide={() => setShowModalNoUser(false)} dialogClassName='modal-90w' aria-labelledby='example-custom-modal-styling-title' className={`${s.modalContainer}`}>
+				<Modal.Header closeButton>
+					<Modal.Title className={`${s.modalTitle}`}>El usuario ingresado no se encuentra registrado</Modal.Title>
 				</Modal.Header>
 				<Modal.Body className={s.cont}>
 					<p>Para poder ingresar primero debes registrarte.</p>
-					<di className={s.button}>
-						<Button className={s.button1}>
+					<div className={`${s.contBotones} d-flex flex-column justify-content-center align-items-center`}>
+						<Button className={`${s.button1} mt-3`} as={Link} to={`/users`}>
+							Crear una cuenta
+						</Button>
+						<Button className={`${s.button2} mt-3`}>
 							{/* Ingreso por passport */}
 							<a href='http://localhost:3001/users/auth/google'>Registrarse con Google</a>
 						</Button>
-						<Button className={s.button2} as={Link} to={`/users`}>
-							Crear cuenta independiente
-						</Button>
-					</di>
+					</div>
 				</Modal.Body>
 			</Modal>
 		</div>
@@ -30,17 +30,17 @@ export const LoginModalNoUser = ({ showModalNoUser, setShowModalNoUser }) => {
 export const LoginModalAuthError = ({ showModalAuthError, setShowModalAuthError }) => {
 	return (
 		<div>
-			<Modal centered show={showModalAuthError} onHide={() => setShowModalAuthError(false)} dialogClassName='modal-90w' aria-labelledby='example-custom-modal-styling-title'>
+			<Modal centered show={showModalAuthError} onHide={() => setShowModalAuthError(false)} dialogClassName='modal-90w' aria-labelledby='example-custom-modal-styling-title' className={`${s.modalContainer}`}>
 				<Modal.Header className={s.title} closeButton>
-					<Modal.Title>Hubo en error con tus credenciales de Google</Modal.Title>
+					<Modal.Title className={`${s.modalTitle}`}>Hubo en error con tus credenciales de Google</Modal.Title>
 				</Modal.Header>
 				<Modal.Body className={s.cont}>
 					<p>Por favor, intentalo de nuevo</p>
-					<di className={s.button}>
-						<Button className={s.button2} onClick={() => setShowModalAuthError(false)}>
+					<div className={`${s.contBotones} d-flex flex-column justify-content-center align-items-center`}>
+						<Button className={s.button1} onClick={() => setShowModalAuthError(false)}>
 							Entendido
 						</Button>
-					</di>
+					</div>
 				</Modal.Body>
 			</Modal>
 		</div>
